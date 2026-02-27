1804d702d9338f7298a34843c3a1cce3f5fb5406
Nickandro96 <nickandroklauss@gmail.com>
2026-02-27 16:21:20 +0100

    Create server-types.ts

---
+++ client/src/server-types.ts
@@ -0,0 +1,9 @@
+/**
+ * Standalone frontend type shim.
+ *
+ * If you keep frontend & backend in a single monorepo, replace this file by:
+ *   export type { AppRouter } from "<path-to-backend>/server/routers";
+ */
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type AppRouter = any;
